4af309546f0d71993c7af791133dabf1fdcb4569
Stefan Pettersson <stefan.pettersson@lumano.se>
2016-11-16 17:55:54 +0100

    Import people + form edits.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ApplicationRecord
-  validates_length_of :first_name, in: 2..50
+  validates_length_of :first_name, in: 1..50
   validates_length_of :last_name, in: 2..50
   #validates_length_of :street, in: 2..100, allow_nil: true
   #validates_length_of :zip, in: 7..32, allow_nil: true
