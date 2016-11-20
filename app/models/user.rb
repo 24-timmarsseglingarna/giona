5fde62d02586b13f6bd518599d1287ed30e9fa7e
Stefan Pettersson <stefan.pettersson@lumano.se>
2016-11-20 14:20:17 +0000

    Basic roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ApplicationRecord
+
+  enum role: [:user, :assistant, :organizer, :admin]
+
+  after_initialize :set_default_role, :if => :new_record?
+
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, 
          :recoverable, :rememberable, :trackable, :validatable
          # :confirmable
+
+
+  def set_default_role
+    self.role = :user
+  end
+
 end
