eacd962dc373448daf65f493a84c8434550763b9
Stefan Pettersson <stefan.pettersson@lumano.se>
2017-02-20 10:52:08 +0000

    Allow CORS.

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,13 @@ module Giona
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Europe/Stockholm'
+
+    config.middleware.insert_before 0, Rack::Cors, :debug => true, :logger => Rails.logger do #TODO tighten up
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
   end
 end
