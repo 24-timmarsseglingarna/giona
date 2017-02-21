7e0da86c753e935f467168cf93c381d7bb34022b
Stefan Pettersson <stefan.pettersson@lumano.se>
2017-02-21 19:42:19 +0000

    Moved console permissions to dev env

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Giona
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Europe/Stockholm'
-    config.web_console.whitelisted_ips = '10.0.2.2'
     config.middleware.insert_before 0, Rack::Cors, :debug => true, :logger => Rails.logger do #TODO tighten up
       allow do
         origins '*'
