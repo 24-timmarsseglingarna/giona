5322fdd012061efa487f3a5467c9bcf7534cfaa0
Stefan Pettersson <stefan.pettersson@lumano.se>
2017-03-16 09:16:51 +0000

    simple authorization of people#show

--- app/controllers/api/v1/people_controller.rb
+++ app/controllers/api/v1/people_controller.rb
@@ -2,13 +2,18 @@ module Api
   module V1
     class PeopleController < ApiController
       acts_as_token_authentication_handler_for User
-      
+
       def index
         respond_with Person.all
       end
 
       def show
-        respond_with Person.find(params[:id])
+        if current_user
+          @person = Person.find(params[:id])
+        else
+          @person = Person.select("id, first_name, last_name").find(params[:id])
+        end
+        render :json => @person
       end
     end
   end
