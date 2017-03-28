feb318b7afcf135ee53c8ccb6ef42a595373deb3
Stefan Pettersson <stefan.pettersson@lumano.se>
2017-03-28 08:52:29 +0000

    Ongoing work to prevent redirect after API sign in.

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,8 @@ Rails.application.routes.draw do
     end
   end
 
+  #devise_for :users, :controllers => { :sessions => 'users/sessions'}
+
   resources :people do
   	collection do
       get 'inactive'
