4bc057ea068e598f8d784fef15a2dee483647e34
Stefan Pettersson <stefan.pettersson@lumano.se>
2017-03-28 08:21:57 +0000

    Added name on races.

--- app/models/race.rb
+++ app/models/race.rb
@@ -10,10 +10,6 @@ class Race < ApplicationRecord
 
   validate :check_start_period
 
-  def name
-  	self.regatta.name + ', period: ' + self.period.to_s + ', start från: ' + I18n.l(self.start_from)
-  end
-
   def check_start_period
     errors.add(:base, "end date should be greater than start") if self.start_from > self.start_to
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328074302) do
+ActiveRecord::Schema.define(version: 20170328080806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,7 @@ ActiveRecord::Schema.define(version: 20170328074302) do
     t.integer  "regatta_id"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.string   "name"
   end
 
   create_table "regattas", force: :cascade do |t|
