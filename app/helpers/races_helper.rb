70f0d1c0b7779c143936bf7773c0188987d3c9c0
Stefan Pettersson <stefan.pettersson@lumano.se>
2017-03-31 07:19:08 +0000

    Ongoing work to improve team application. To be continued.

--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -16,4 +16,23 @@ module RacesHelper
   	out
   end # def race_brief
 
+  def race_start race
+    # Exact start moment
+    # Period, same day
+    # Period, over days
+
+    out = I18n.l(race.start_from, format: :long)
+    if race.start_from. != race.start_to
+      out += ' – '
+      if race.start_from.to_date == race.start_to.to_date 
+        out += I18n.l(race.start_to, format: :clock)
+      else
+        out += I18n.l(race.start_to, format: :short)
+      end
+    end
+    out
+  end # def race_brief
+
+
+
 end
