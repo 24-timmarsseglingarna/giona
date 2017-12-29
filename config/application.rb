bb443d77fc9901fb2491b7ba9b0d814b2d3c5b3d
Martin Björklund <mbj4668@gmail.com>
2017-12-29 17:25:08 +0100

    send etag, accept patch

--- config/application.rb
+++ config/application.rb
@@ -8,17 +8,20 @@ Bundler.require(*Rails.groups)
 
 module Giona
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over
+    # those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Europe/Stockholm'
-    config.middleware.insert_before 0, Rack::Cors, :debug => true, :logger => Rails.logger do #TODO tighten up
+    config.middleware.insert_before 0, Rack::Cors, :debug => true,
+                                    :logger => Rails.logger do #TODO tighten up
       allow do
         origins '*'
-        resource '*', 
-        :headers => :any, 
-        :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-        :methods => [:get, :post, :options, :delete, :put]
+        resource '*',
+        :headers => :any,
+        :expose  => ['access-token', 'etag', 'expiry',
+                     'token-type', 'uid', 'client'],
+        :methods => [:get, :post, :options, :delete, :put, :patch]
       end
     end
 
