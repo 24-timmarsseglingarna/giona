04e2dcb7ae99bc8bf35c289cf988b51ddd8e2c4d
Martin Björklund <mbj4668@gmail.com>
2026-02-24 17:10:37 +0100

    better maraton list view

--- app/controllers/marathon_controller.rb
+++ app/controllers/marathon_controller.rb
@@ -1,47 +1,48 @@
 class MarathonController < ApplicationController
   def index
-    @year        = params[:year].present? ? params[:year].to_i : nil
+    @year = params[:year].present? ? params[:year].to_i : nil
     @organizer_id = params[:organizer_id].present? ? params[:organizer_id].to_i : nil
-    @organizers  = Organizer.marathon_eligible
+    @organizers = Organizer.marathon_eligible
 
     logs = MarathonLog.all
-    logs = logs.where(year: @year)         if @year
+    logs = logs.where(year: @year) if @year
     logs = logs.where(organizer_id: @organizer_id) if @organizer_id
 
-    # Group logs by marathon_person_id in Ruby to avoid N+1
-    grouped = logs.group_by(&:marathon_person_id)
-    all_person_ids = grouped.keys
+    # Person IDs matched by the active filters
+    filtered_person_ids = logs.distinct.pluck(:marathon_person_id)
 
-    marathon_people = MarathonPerson.where(id: all_person_ids).index_by(&:id)
+    marathon_people = MarathonPerson.where(id: filtered_person_ids).index_by(&:id)
 
-    # For new_plaques we need totals up to (year - 1) for each person.
-    # Load all logs for those persons once.
-    all_logs_for_persons = MarathonLog.where(marathon_person_id: all_person_ids)
+    # Load ALL logs for those persons (to compute all-time totals and latest entry)
+    all_logs_for_persons = MarathonLog.where(marathon_person_id: filtered_person_ids)
                                       .group_by(&:marathon_person_id)
 
-    @rows = all_person_ids.map do |mp_id|
+    organizers_by_id = Organizer.all.index_by(&:id)
+
+    @rows = filtered_person_ids.map do |mp_id|
       mp = marathon_people[mp_id]
       next unless mp
 
-      dist_this_year  = grouped[mp_id].sum(&:plaque_dist)
-      all_logs        = all_logs_for_persons[mp_id] || []
-      total_dist      = all_logs.sum(&:plaque_dist)
-      dist_prev_years = total_dist - dist_this_year
+      all_logs          = all_logs_for_persons[mp_id] || []
+      latest            = all_logs.max_by(&:year)
+      total_plaque_dist = all_logs.sum(&:plaque_dist)
 
       if @year
         prev_total = all_logs.select { |l| l.year < @year }.sum(&:plaque_dist)
-        new_p      = MarathonThresholds.new_plaques(prev_total, total_dist)
+        new_p      = MarathonThresholds.new_plaques(prev_total, total_plaque_dist)
       else
         new_p = []
       end
 
       {
-        marathon_person:  mp,
-        dist_this_year:   dist_this_year,
-        dist_prev_years:  dist_prev_years,
-        total_plaque_dist: total_dist,
-        current_plaque:   MarathonThresholds.current_plaque(total_dist),
-        new_plaques:      new_p
+        marathon_person:   mp,
+        total_sailed_dist: all_logs.sum(&:sailed_dist),
+        total_plaque_dist: total_plaque_dist,
+        latest_boat_name:  latest&.boat_name,
+        latest_boat_type:  latest&.boat_type,
+        latest_year:       latest&.year,
+        latest_organizer:  organizers_by_id[latest&.organizer_id],
+        new_plaques:       new_p
       }
     end.compact
 
